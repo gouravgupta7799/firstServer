d0f0543999e3026b676be708899b7f3b6f32fe1c
Gourav Gupta <gouravgupta7799@gmail.com>
2023-02-21 19:13:08 +0530

    chages1

--- firstserver.js
+++ firstserver.js
@@ -1,7 +1,40 @@
 let http = require('http');
-
-const server = http.createServer((req, res)=> {
-  console.log("Gourav Gupta")
+const server = http.createServer((req, res) => {
+  const url = req.url
+  if (url === '/') {
+    console.log("Gourav Gupta")
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>this is my first server</title></head>')
+    res.write('<body><form action="/message" method="post"><input type ="text" name="message"><button type="submit">submit</button></input></form></body>')
+    res.write('<h1>"Welcome to my Node Js project"</h1>')
+    res.write('</html>')
+    return res.end()
+  }
+  else if (url === '/home') {
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>this is my first server</title></head>')
+    res.write('<body><h1> "Welcome to my home"</h1></body > ')
+    res.write('</html>')
+    res.end()
+  }
+  else if (url === '/about') {
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>this is my first server</title></head>')
+    res.write('<body><h1>"Welcome to About Us page"</h1></body > ')
+    res.write('</html>')
+    res.end()
+  }
+  else if (url === '/node') {
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>this is my first server</title></head>')
+    res.write('<body><h1>"Welcome to my Node Js project"</h1></body > ')
+    res.write('</html>')
+    res.end()
+  }
 });
 
 server.listen(4000);
